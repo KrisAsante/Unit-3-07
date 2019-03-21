d111da8c461320b92f6defc2a674878d8422c80b
Chris <36240719+KrisAsante@users.noreply.github.com>
2019-03-21 14:49:31 -0400

    Add files via upload

---
+++ 3-07.py
@@ -0,0 +1,14 @@
+# Created by: Chris Asante
+# Created on: 19-March-2019
+# Created for: ICS3U
+# Daily Assignment – Unit 3-07  n n
+# This program is an application that calculates the diameter of a pizza
+
+input = input()
+test = int(input)
+answer = 1
+for i in range(test):
+    answer = (i+1)*answer
+    
+    
+print (answer)
